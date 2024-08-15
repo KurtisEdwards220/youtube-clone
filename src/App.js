6bc2425e0667629a09d99c8e4721cd5262e17308
Kurtis Edwards <edwards.kurtis@gmail.com>
2024-08-15 17:23:51 -0600

    chore: Update BrowserRouter basename to use GitHub Pages URL

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
 
 function App() {
   return (
-    <BrowserRouter basename="/youtube-clone">
+    <BrowserRouter basename="https://kurtisedwards220.github.io/youtube-clone/">
       <Box sx={{ backgroundColor: '#000' }}>
         <Navbar />
         <Routes>
