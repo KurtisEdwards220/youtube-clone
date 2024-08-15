b963cf0d1aaf56b1be0ab6b36791ca656025e265
Kurtis Edwards <edwards.kurtis@gmail.com>
2024-08-14 20:29:55 -0600

    refactor: Update VideoCard component to improve responsiveness and image loading

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -19,14 +19,20 @@ function VideoCard({
   return (
     <Card
       sx={{
-        width: { md: '320px', xs: '100%', boxShadow: 'none', boarderRadius: 0 },
+        width: {
+          xs: '100%',
+          sm: '358px',
+          md: '320px',
+          boxShadow: 'none',
+          boarderRadius: 0,
+        },
       }}
     >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
